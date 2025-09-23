9791e51b489ce505a055cdd1eb8d57b421e291b5
lerunnersrp-sudo <lerunnersrp@gmail.com>
2025-09-23 15:48:37 -0300

    Update and rename app.js to js/config.js

---
+++ js/config.js
@@ -0,0 +1,18 @@
+// ===================================================================
+// ARQUIVO DE CONFIGURAÇÃO - Contém as chaves de conexão do projeto.
+// ===================================================================
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDGLozMAqDoQdP_znYi3mbebUomE-_O6hU",
+    authDomain: "lerunners.firebaseapp.com",
+    databaseURL: "https://lerunners-default-rtdb.firebaseio.com",
+    projectId: "lerunners",
+    storageBucket: "lerunners.firebasestorage.app",
+    messagingSenderId: "786096020973",
+    appId: "1:786096020973:web:334dc555218cedb0e1dbe4"
+};
+
+const CLOUDINARY_CONFIG = {
+    CLOUD_NAME: "dd6ppm6nf",
+    API_KEY: "845911223412467",
+    API_SECRET: "S6YefZx7J5StgcTV-greU4wFhP4"
+};
